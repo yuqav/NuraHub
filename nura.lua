464781c0e95074aa4107c5569065ed0cb5890800
yuqav <tronyax@gmail.com>
2024-12-11 20:17:17 +0300

    Update nura.lua

--- nura.lua
+++ nura.lua
@@ -1,6 +1,8 @@
 local mouse = game.Players.LocalPlayer:GetMouse()
 mouse.Icon = "rbxassetid://11716159644"
 
+local autoRoll = false
+
 local Lib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
 local Window = Lib:MakeWindow({
@@ -26,14 +28,41 @@ local tab3 = Window:MakeTab({
 })
 
 local tab4 = Window:MakeTab({
-    Name = "Jump Pre Second",
+    Name = "Mount RNG",
     Icon = "rbxassetid://7999345313",
     PremiumOnly = false
 })
+
+function autoR()
+    while true do
+        task.wait()
+        game:GetService("ReplicatedStorage").Remotes.RollFunction:InvokeServer()
+    
+        local r = game:GetService("ReplicatedStorage")
+        local m = r:FindFirstChild("Mounts")
+    
+        if m then
+            for _, i in pairs(m:GetChildren()) do
+                r.Remotes.SpawnMount:InvokeServer(i.Name)
+            end
+        end
+    end
+end
 
 tab3:AddButton({
     Name = "Inf Money",
     Callback = function()
         game.ReplicatedStorage.fewjnfejwb3:FireServer(999999999999999999)
+    end
+})
+
+tab4:AddToggle({
+    Name = "Auto Roll",
+    Default = false,
+    Callback = function(v)
+        autoRoll = v
+        if autoRoll then
+            autoR
+        end
     end
 })
